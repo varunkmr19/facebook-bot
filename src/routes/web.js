c45d5e09034e776e67eda02390fb1d0b5edef761
Varun Kumar <varun-kumar@live.com>
2022-02-05 13:15:03 +0530

    fb webhook initial setup

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,10 +1,13 @@
 import express from "express";
+import chatBotController from "../controllers/chatBotController";
 import homeController from "../controllers/homeController";
 
 const router = express.Router();
 
 const initWebRoutes = (app) => {
   router.get("/", homeController.getHomePage);
+  router.get("/webhook", chatBotController.getWebHook)
+  router.post("/webhook", chatBotController.postWebHook)
   return app.use('/', router);
 };
 
